098bce95965bb998f2e8d76ad5595a8ec3fe726e
Jonathan Prust Vernizzi <jonathan@flipfloplab.com.br>
2022-09-19 17:56:05 -0300

    feat: added /jobs/:id/pay to enable paying for a job

--- src/app.js
+++ src/app.js
@@ -73,4 +73,106 @@ app.get('/contracts/:id',getProfile ,async (req, res) =>{
     res.json(jobs)
 })
 
+/**
+ * @returns status of the payment operation
+ */
+ app.post('/jobs/:id/pay',getProfile ,async (req, res) =>{
+    const {Job, Contract, Profile} = req.app.get('models');
+    const {id} = req.params;
+    const job = await Job.findOne({
+        where : {
+            paid: null,
+            id,
+        },
+        include: {
+            model: Contract,
+            where: {
+                [Op.or]: [
+                    {ClientId: req.profile.id},
+                ],
+            }
+        }
+    })
+
+    if (!job) {
+        return res.status(404).json({
+            status: 'Error',
+            message: 'This job has already been paid or doesn\'t belong to this user.',
+        });
+    }
+
+    // Job exists and is pending payment, process payment now..
+
+    if (req.profile.balance < job.price) {
+        return res.status(409).json({
+            status: 'Error',
+            message: 'Insufficient balance to process payment',
+        });
+    }
+
+    const transaction = await sequelize.transaction();
+
+    try {
+        
+        const client = await Profile.findOne({
+            where: {
+                id: req.profile.id,
+            },
+            lock: true,
+            transaction,
+        });
+
+        const promises = [];
+
+        promises.push(Profile.update(
+            { balance: client.balance - job.price },
+            { 
+                where : { 
+                    id : client.id,
+                    balance: {
+                        [Op.gte] : job.price,
+                    }
+                },
+                transaction,
+            }
+        ));
+
+        promises.push(Profile.increment('balance', {
+            by: job.price, 
+            where: {
+                id: job.Contract.ContractorId,
+            },
+            transaction,
+        }));
+
+        promises.push(Job.update(
+            {
+                paid: 1,
+                paymentDate: new Date(),
+            },
+            {
+                where: {
+                    id: job.id,
+                },
+                transaction,
+            }
+        ));
+
+        await Promise.all(promises);
+
+        await transaction.commit();
+    } catch (error) {
+        await transaction.rollback();
+        res.status(409).json({
+            status: 'Error',
+            message: 'Unable to process payment.',
+        })
+    }
+
+    res.json({
+        status: 'Ok',
+        message: 'Job successfully paid.',
+    });
+})
+
 module.exports = app;
